e9c18052a76a81abbe788aebfca915c88b9c195f
IbnuRizalKhusaini <ibnurizal02nov@gmail.com>
2023-02-21 10:00:14 +0700

    Pertemuan2

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name" : "New Addons",
+    "version" : "1.0",
+    "author" : "Ibnu Rizal Khusaini",
+    "website" : "https://www.detik.com/",
+    "category" : "New Module",
+    "summary" : "Ini adalah Addons baru",
+    "description" : """
+        Latihan membuat addons baru di odoo 16
+    """,
+    "depends" : [
+        "base",
+    ],
+    "data" : [
+
+    ],
+    "demo" : [],
+    "test" : [],
+    "image" : [],
+    "qweb" : [],
+    "css" : [],
+    "application" : True,
+    "installable" : True,
+    "auto_install" : False,
+}
